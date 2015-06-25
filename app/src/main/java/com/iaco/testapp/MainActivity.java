ab75b7f8bdd658e8c36047ef0b3932945112e54e
Othon <othonic@gmail.com>
2015-06-24 22:45:45 -0500

    Use custom view Pager (Actionbar tabs are deprecated)

--- app/src/main/java/com/iaco/testapp/MainActivity.java
+++ app/src/main/java/com/iaco/testapp/MainActivity.java
@@ -7,29 +7,87 @@ package com.iaco.testapp;
 
 import com.iaco.testapp.R;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends FragmentActivity 
 {
 
+	public static class TabPageAdapter extends SmartFragmentStatePagerAdapter {
+		private static int NUM_ITEMS = 2;
+
+		public TabPageAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
+		}
+
+		// Returns total number of pages
+		@Override
+		public int getCount() {
+			return NUM_ITEMS;
+		}
+
+		// Returns the fragment to display for that page
+		@Override
+		public Fragment getItem(int position) {
+			switch (position) {
+				case 0: // Fragment # 0 - This will show FirstFragment
+					return new ItemListFragment();
+				case 1: // Fragment # 0 - This will show FirstFragment different title
+					return new EditItemListFragment();
+
+				default:
+					return null;
+			}
+		}
+
+		// Returns the page title for the top indicator
+		@Override
+		public CharSequence getPageTitle(int position) {
+
+			switch (position) {
+				case 0: // Fragment # 0 - This will show FirstFragment
+					return "Shopping";
+				case 1: // Fragment # 0 - This will show FirstFragment different title
+					return "Edit List";
+
+				default:
+					return null;
+			}
+
+		}
+
+	}
+
 	boolean _firstTime = true;
 	int fragmentId = 0;
+
+	TabPageAdapter pageAdapter = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		CustomViewPager pager = (CustomViewPager) findViewById(R.id.pager);
 
-		showShop();
 
+		pageAdapter = new TabPageAdapter(getSupportFragmentManager());
+		pager.setAdapter(pageAdapter);
+		pager.setPagingEnabled(false);
 
+		//showShop();
+
+		pager.setCurrentItem(0);
 
 	}
 
@@ -46,11 +104,11 @@ public class MainActivity extends FragmentActivity
 
 		switch (item.getItemId()) {
 			case R.id.action_shop: {
-				showShop();
+			//	showShop();
 				return true;
 			}
 			case R.id.action_edit: {
-				showEdit();
+			//	showEdit();
 				return true;
 			}
 		}
@@ -61,7 +119,7 @@ public class MainActivity extends FragmentActivity
 	void showShop()
 	{
 
-
+		/*
 		if(_firstTime) {
 			getSupportFragmentManager().beginTransaction().add(
 					R.id.ActivityMain_Layout,
@@ -80,12 +138,13 @@ public class MainActivity extends FragmentActivity
 
 		}
 		_firstTime=false;
-
+		*/
 	}
 
 
 	void showEdit()
 	{
+		/*
 		if(fragmentId !=1) {
 			fragmentId = 1;
 			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -93,6 +152,7 @@ public class MainActivity extends FragmentActivity
 			transaction.addToBackStack(null);
 			transaction.commit();
 		}
+		*/
 	}
 
 
---
+++ app/src/main/java/com/iaco/testapp/SmartFragmentStatePagerAdapter.java
@@ -0,0 +1,52 @@
+package com.iaco.testapp;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.util.SparseArray;
+import android.view.Menu;
+import android.view.ViewGroup;
+
+/**
+ * Created by macbook on 6/24/15.
+ */
+/*
+   Extension of FragmentStatePagerAdapter which intelligently caches
+   all active fragments and manages the fragment lifecycles.
+   Usage involves extending from SmartFragmentStatePagerAdapter as you would any other PagerAdapter.
+*/
+public abstract class SmartFragmentStatePagerAdapter extends FragmentStatePagerAdapter {
+    // Sparse array to keep track of registered fragments in memory
+    private SparseArray<Fragment> registeredFragments = new SparseArray<Fragment>();
+
+    public SmartFragmentStatePagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    // Register the fragment when the item is instantiated
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+
+    // Unregister when the item is inactive
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
+
+    // Returns the fragment for the position (if instantiated)
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
+    }
+}
